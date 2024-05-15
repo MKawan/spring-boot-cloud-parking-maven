1da3cf2f4dad9ffcc47ee18dd5ba88739bcea8a8
Mateus Kawan <mateus77kawan@gmail.com>
2024-05-15 19:24:59 -0300

    up project cloud parking

---
+++ src/main/java/com/diospringboot/parking/SpringBootClaudParkingMavenApplication.java
@@ -0,0 +1,13 @@
+package com.diospringboot.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClaudParkingMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootClaudParkingMavenApplication.class, args);
+	}
+
+}
